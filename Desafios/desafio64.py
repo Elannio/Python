8774a243164486d0f2ef1f19a34ed615f2608ce1
Elannio <elanniosousa18@gmail.com>
2023-08-31 21:04:01 -0300

    Adicionei as questões 64 e 65

--- Desafios/desafio64.py
+++ Desafios/desafio64.py
@@ -3,3 +3,14 @@ pelo teclado. O programa vai parar quando o usuário
 digitar o valor 999, que é a condição de parada. 
 No final, mostre quantos números foram digitados e qual 
 foi a soma entre eles (Desconsiderando o flag)'''
+
+num = 0
+cont = 1
+soma = 0
+
+while num != 999:
+    num = int(input("Digite um número, quando você digitar 999 irei parar de pedir."))
+    cont += 1
+    soma += num
+print (f"O total de números que você digitou foi {cont} e a soma entre eles foi de {soma-999}")
+    
--- Desafios/desafio65.py
+++ Desafios/desafio65.py
@@ -3,3 +3,29 @@ teclado. No final, mostre a média entre todos os valores
  e qual foi o maior e o menor valores lidos. O programa 
  deve perguntar ao usuário se ele quer ou não continuar 
  a digitar valores.'''
+ 
+media = 0
+maior = float('-inf')  # Iniciar com um valor muito baixo para encontrar o maior
+menor = float('inf')   # Iniciar com um valor muito alto para encontrar o menor
+soma = 0
+cont = 0  # Iniciar o contador em 0
+
+while True:
+    num = int(input("Digite um número. Se quiser parar, digite 0: "))
+    
+    if num == 0:
+        break  # Sai do loop quando o usuário digitar 0
+    
+    cont += 1
+    soma += num
+    
+    if num > maior:
+        maior = num
+    
+    if num < menor:
+        menor = num
+
+if cont > 0:
+    media = soma / cont
+
+print(f"A média dos valores é de {media}, o maior número é {maior}, o menor número é {menor}.")
